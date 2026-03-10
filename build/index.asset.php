a9e3d7194ead24424378dad6aadeb225d6b874fd
Carsten Bach <mail@carsten-bach.de>
2026-03-10 23:25:32 +0100

    Update german translation

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => '91fcd158bbc7d109a512');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-element', 'wp-i18n'), 'version' => 'ed0888c64e1ad2925555');
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '1f854d5b598ef9c9a105');
+<?php return array('dependencies' => array(), 'version' => 'ac5cf644bbb2b0ffa0f4');
